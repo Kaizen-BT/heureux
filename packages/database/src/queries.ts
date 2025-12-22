f0655bc1a33e4f8d4423800ef30584dd33a5931f
alias-ktc <alenpeni.info@gmail.com>
2025-12-22 14:02:34 +1000

    Added schemas & fetch query runners

--- packages/database/src/queries.ts
+++ packages/database/src/queries.ts
@@ -1,15 +1,51 @@
 import type { AppDatabase } from "./index.js";
+import type { MilestoneSelect, ProjectSelect, TaskSelect } from "./schema.js";
+
+// --- Interfaces & Type Aliases ---
 
 interface QueryFuncBase {
   database: AppDatabase;
 }
 
+type QueryResult<T> = Promise<T>;
+
+// --- Query Runners ---
+
 /**
  * Fetches all the projects from the AppDatabase
  *
  * @param {QueryFuncBase} props
+ * @returns {QueryResult<ProjectSelect[]>}
  */
-export async function getAllProjects({ database }: QueryFuncBase) {
+export async function getAllProjects({
+  database,
+}: QueryFuncBase): QueryResult<ProjectSelect[]> {
   const data = await database.query.projects.findMany();
+  return data;
+}
+
+/**
+ * Fetches all the tasks in the AppDatabase
+ *
+ * @param {QueryFuncBase} props
+ * @returns {QueryResult<TaskSelect[]>}
+ */
+export async function getAllTasks({
+  database,
+}: QueryFuncBase): QueryResult<TaskSelect[]> {
+  const data = await database.query.tasks.findMany();
+  return data;
+}
+
+/**
+ * Fetches all the milestones in the AppDatabase
+ *
+ * @param {QueryFuncBase} props
+ * @returns {QueryResult<MilestoneSelect[]>}
+ */
+export async function getAllMilestones({
+  database,
+}: QueryFuncBase): QueryResult<MilestoneSelect[]> {
+  const data = await database.query.milestones.findMany();
   return data;
 }
