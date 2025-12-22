fe830a68b23e22989f9f7b58f6488c5b41966810
alias-ktc <alenpeni.info@gmail.com>
2025-12-22 19:25:50 +1000

    Readjusted schema and working on IPC

---
+++ packages/main/src/utils/ipcMainWrapper.ts
@@ -0,0 +1,44 @@
+import { APIContract } from "@app/api";
+import {
+  AppDatabase,
+  getAllMilestones,
+  getAllProjects,
+  getAllTasks,
+} from "@app/database";
+import { ipcMain } from "electron";
+
+/**
+ * A wrapper around ipcMain.handle that registers a channel and an event
+ * listener with the channel and types inferred from the APIContract
+ */
+export function mainHandleAPI<K extends keyof APIContract>(
+  channel: K,
+  handler: (...args: Parameters<APIContract[K]>) => ReturnType<APIContract[K]>
+) {
+  ipcMain.handle(channel, async (_, args) => {
+    return await handler(...(args as Parameters<APIContract[K]>));
+  });
+}
+
+/**
+ * Initializes the Query driver which is an object that abstracts the
+ * Database query functions
+ */
+export function initializeQueryDriver({
+  database,
+}: {
+  database: AppDatabase;
+}): APIContract {
+  const driver = {
+    // Projects
+    getAllProjects: () => getAllProjects({ database }),
+
+    // Milestones
+    getAllMilestones: () => getAllMilestones({ database }),
+
+    // Tasks
+    getAllTasks: () => getAllTasks({ database }),
+  } satisfies APIContract;
+
+  return driver;
+}
